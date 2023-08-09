2c498b4e49d677108d9d3a475c9ba99d9b86d999
vrishtik2007 <vrishtik2007@gmail.com>
2023-08-09 10:13:45 -0400

    Initial Commit

---
+++ Intro to Xcode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Intro to Xcode
+//
+//  Created by Megha Kulkarni on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Intro to Xcode/Intro_to_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intro_to_XcodeApp.swift
+//  Intro to Xcode
+//
+//  Created by Megha Kulkarni on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Intro_to_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
